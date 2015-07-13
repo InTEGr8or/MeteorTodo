3433c8734dd5b52eb178544b0e4c78ad6fa6276e
InTEGr8or <admin@thefullertonian.com>
2015-07-12 20:31:40 -0700

    Temporary UI state

--- MeteorTodo.js
+++ MeteorTodo.js
@@ -3,7 +3,17 @@ if (Meteor.isClient) {
   //This code only runs on the client.
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true }}).count();
     }
   })
   Template.body.events({
@@ -15,6 +25,17 @@ if (Meteor.isClient) {
       });
       event.target.text.value = "";
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  })
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
     }
   })
 }
